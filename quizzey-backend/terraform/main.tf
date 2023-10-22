d036070ef1af3f12a0dcbde565b78e1287bc48b9
Juwan Gross <juwangross0815@gmail.com>
2023-10-22 14:09:06 -0400

    Added data sources needed for lambdas. Added s3 enpoint url in Copy lambdas to S3 step

--- quizzey-backend/terraform/main.tf
+++ quizzey-backend/terraform/main.tf
@@ -20,7 +20,41 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  subnets = ["Lambda Subnet 1", "Lambda Subnet 2"]
+}
 
+
+#  -------------------   Begin Networking  ---------------------------
+# querying quizzey vpc info
+data "aws_vpc" "quizzey_vpc" {
+  filter {
+    name = "tag:Name"
+    values = ["Quizzey VPC"]
+  }
+}
+
+# querying lambda security groups in quizzey vpc
+data "aws_security_groups" "lambda_sg" {
+  filter {
+    name = "group-name"
+    values = ["quizzey_lambda_sg"]
+  }
+}
+
+# querying lambda subnets in quizzey vpc
+data "aws_subnets" "lambda_subnets" {
+  filter {
+    name = "vpc-id"
+    values = [data.data.aws_vpc.quizzey_vpc.id] 
+  }
+
+  filter {
+    name = "tag:Name"
+    values = local.subnets
+  }
+}
+#  -------------------   End Networking  ---------------------------
 
 
 resource "aws_iam_role" "iam_role_for_lambda" {
@@ -104,6 +138,11 @@ resource "aws_lambda_function" "courses_get_lambda" {
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.courses_getter_handler"
   runtime          = "python3.10"
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.lambda_subnets.ids
+    security_group_ids = data.aws_security_groups.lambda_sg.ids
+  }
 }
 
 
@@ -129,5 +168,10 @@ resource "aws_lambda_function" "ind_course_get_lambda" {
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.course_getter_handler"
   runtime          = "python3.10"
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.lambda_subnets.ids
+    security_group_ids = data.aws_security_groups.lambda_sg.ids
+  }
 }
 
