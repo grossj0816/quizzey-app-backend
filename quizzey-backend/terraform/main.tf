761373139a03f7deb7b3b7cfa86a7c213ced1770
Juwan Gross <juwangross0815@gmail.com>
2023-10-22 21:29:21 -0400

    commit

--- quizzey-backend/terraform/main.tf
+++ quizzey-backend/terraform/main.tf
@@ -138,6 +138,7 @@ resource "aws_lambda_function" "courses_get_lambda" {
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.courses_getter_handler"
   runtime          = "python3.10"
+  architectures    = ["arm64"]  
 
   # vpc_config {
   #   subnet_ids = data.aws_subnets.lambda_subnets.ids
@@ -168,6 +169,7 @@ resource "aws_lambda_function" "ind_course_get_lambda" {
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.course_getter_handler"
   runtime          = "python3.10"
+  architectures    = ["arm64"]  
 
   # vpc_config {
   #   subnet_ids = data.aws_subnets.lambda_subnets.ids
