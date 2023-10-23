bb6dbf14ff6833245016a90ab554b6e5501e99e2
Juwan Gross <juwangross0815@gmail.com>
2023-10-22 20:53:59 -0400

    removed etag

--- quizzey-backend/terraform/main.tf
+++ quizzey-backend/terraform/main.tf
@@ -112,6 +112,7 @@ resource "aws_s3_object" "quizzey-object" {
   bucket    = "tu-api-lambda-deploys"
   key       = "quizzey_app/lambdas.zip"
   source    = "../lambdas/lambdas.zip"
+  # etag      = filemd5("../lambdas/lambdas.zip")
 }
 
 
@@ -133,7 +134,7 @@ resource "aws_lambda_function" "courses_get_lambda" {
   s3_bucket        = "tu-api-lambda-deploys"
   s3_key           = "quizzey_app/lambdas.zip"
   function_name    = "fetch_all_courses"
-  source_code_hash = filebase64sha256("../lambdas/lambdas.zip")
+  source_code_hash = filebase64sha256("quizzey_app/lambdas.zip")
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.courses_getter_handler"
   runtime          = "python3.10"
@@ -163,7 +164,7 @@ resource "aws_lambda_function" "ind_course_get_lambda" {
   s3_bucket        = "tu-api-lambda-deploys"
   s3_key           = "quizzey_app/lambdas.zip"
   function_name    = "fetch_course"
-  source_code_hash = filebase64sha256("../lambdas/lambdas.zip")
+  source_code_hash = filebase64sha256("quizzey_app/lambdas.zip")
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.course_getter_handler"
   runtime          = "python3.10"
