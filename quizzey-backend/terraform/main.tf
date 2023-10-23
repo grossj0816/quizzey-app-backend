daa67e904188dc8979a1768b2e60a0d3a2e56ec7
Juwan Gross <juwangross0815@gmail.com>
2023-10-22 21:15:21 -0400

    commit

--- quizzey-backend/terraform/main.tf
+++ quizzey-backend/terraform/main.tf
@@ -111,7 +111,7 @@ resource "aws_iam_role_policy_attachment" "attach_policy_to_role" {
 resource "aws_s3_object" "quizzey-object" {
   bucket    = "tu-api-lambda-deploys"
   key       = "quizzey_app/lambdas.zip"
-  source    = "../lambdas/lambdas.zip"
+  # source    = "../lambdas/lambdas.zip"
   # etag      = filemd5("../lambdas/lambdas.zip")
 }
 
@@ -134,7 +134,7 @@ resource "aws_lambda_function" "courses_get_lambda" {
   s3_bucket        = "tu-api-lambda-deploys"
   s3_key           = "quizzey_app/lambdas.zip"
   function_name    = "fetch_all_courses"
-  source_code_hash = join("", data.aws_s3_object.quizzey-object.*.body)
+  # source_code_hash = aws_s3_object.quizzey-object.
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.courses_getter_handler"
   runtime          = "python3.10"
@@ -164,7 +164,7 @@ resource "aws_lambda_function" "ind_course_get_lambda" {
   s3_bucket        = "tu-api-lambda-deploys"
   s3_key           = "quizzey_app/lambdas.zip"
   function_name    = "fetch_course"
-  source_code_hash = join("", data.aws_s3_object.quizzey-object.*.body)
+  # source_code_hash = join("", data.aws_s3_object.quizzey-object.*.body)
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = "courses.course_getter_handler"
   runtime          = "python3.10"
