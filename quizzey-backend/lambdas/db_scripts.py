ac99b4694289fcc8aea174a932075c4900541625
Juwan Gross <jgross@excelsior.edu>
2023-11-07 11:16:27 -0500

    Made more updates to create scripts with foreign keys

--- quizzey-backend/lambdas/db_scripts.py
+++ quizzey-backend/lambdas/db_scripts.py
@@ -24,8 +24,8 @@ def all_tables_create_handler(event, context):
             print("Connected to MySQL Server version:", db_info)
 
             cursor.execute("CREATE TABLE IF NOT EXISTS `courses`(`courseId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `courseName` VARCHAR(75), `organization` VARCHAR(150), `textbook` VARCHAR(150), `active` BOOLEAN, `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastViewedDate` DATETIME(0))")
-            cursor.execute("CREATE TABLE IF NOT EXISTS `quizzey_sets`(`setId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, courseId INT NOT NULL FOREIGN KEY (`courseId`) REFERENCES courses(`courseId`), `setName` VARCHAR(175), `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastViewedDate` DATETIME(0))")
-            cursor.execute("CREATE TABLE IF NOT EXISTS `questions`(`questionId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, setId INT NOT NULL FOREIGN KEY (`setId`) REFERENCES quizzey_sets(`setId`), `question` VARCHAR(450), `answer` VARCHAR(450), `createdBy` VARCHAR(100), `createdDate` DATETIME(0))")
+            cursor.execute("CREATE TABLE IF NOT EXISTS `quizzey_sets`(`setId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, courseId INT NOT NULL, `setName` VARCHAR(175), `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastViewedDate` DATETIME(0), FOREIGN KEY(`courseId`) REFERENCES `courses`(`courseId`))")
+            cursor.execute("CREATE TABLE IF NOT EXISTS `questions`(`questionId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, setId INT NOT NULL, `question` VARCHAR(450), `answer` VARCHAR(450), `createdBy` VARCHAR(100), `createdDate` DATETIME(0), FOREIGN KEY (`setId`) REFERENCES `quizzey_sets`(`setId`))")
             print("Table creation has been completed.")
     except Error as e:
         print('Error while connecting to MySQL...', e)
