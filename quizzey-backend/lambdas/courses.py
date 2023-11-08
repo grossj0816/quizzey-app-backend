c0c405695d5bd2ced3d24dcd2a6ddcd27e93141b
Juwan Gross <jgross@excelsior.edu>
2023-11-08 09:53:56 -0500

    Uncommented out rest of code in update course lambda and passed variables in data_for_query.

--- quizzey-backend/lambdas/courses.py
+++ quizzey-backend/lambdas/courses.py
@@ -107,35 +107,36 @@ def create_new_course_handler(event, context):
     print(active)
     print(created_by)
     print(created_date)
-    # try:
 
-    #     connection = mysql.connector.connect(host=host, database=db_name, user=username, password=password)
-    #     cursor = connection.cursor(dictionary=True)
+    try:
 
-    #     if connection.is_connected():
-    #         db_info = connection.get_server_info()
-    #         print("Connected to MySQL Server version:", db_info)
+        connection = mysql.connector.connect(host=host, database=db_name, user=username, password=password)
+        cursor = connection.cursor(dictionary=True)
+
+        if connection.is_connected():
+            db_info = connection.get_server_info()
+            print("Connected to MySQL Server version:", db_info)
             
 
-    #         #Select all records from courses table    
-    #         query = ("INSERT INTO courses"
-    #                  "(courseName, organization, textbook, active, createdBy, createdDate)"
-    #                  "VALUES (%s, %s, %s, %s, %s, %s)") 
+            #Select all records from courses table    
+            query = ("INSERT INTO courses"
+                     "(courseName, organization, textbook, active, createdBy, createdDate)"
+                     "VALUES (%s, %s, %s, %s, %s, %s)") 
 
-    #         data_for_query = ("Human Anatomy & Physiology", "SUNY Cobleskill", "Human Anatomy & Physiology Version 1", True, "SYS-ADMIN", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    #         cursor.execute(query, data_for_query)
+            data_for_query = (course_name, organization, textbook, active, created_by, created_date)
+            cursor.execute(query, data_for_query)
 
-    #         # Commit dada to db
-    #         connection.commit()
-    # except Error as e:
-    #     print('Error while connecting to MySQL...')
-    #     connection.rollback()
-    #     print('Rollbacked db commit due to error...', e)
-    # finally:
-    #     if connection.is_connected():
-    #         cursor.close()
-    #         connection.close()
-    #         print("MySQL connection is closed.")
+            # Commit dada to db
+            connection.commit()
+    except Error as e:
+        print('Error while connecting to MySQL...')
+        connection.rollback()
+        print('Rollbacked db commit due to error...', e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
             
     return{
         "statusCode": 200,
@@ -167,7 +168,7 @@ def update_course_handler(event, context):
             data_for_query = ("Human Anatomy & Physiologysss", "SUNY Cobleskill", "Human Anatomy & Physiology Version 1", True, "SYS-ADMIN", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 1)
             cursor.execute(query, data_for_query)
 
-            # Commit dada to db
+            # Commit data to db
             connection.commit()
     except Error as e:
         print('Error while connecting to MySQL...')
