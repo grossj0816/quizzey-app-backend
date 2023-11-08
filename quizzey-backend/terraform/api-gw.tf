1a7f7ff585d7624217208ae32670273fae621390
Juwan Gross <jgross@excelsior.edu>
2023-11-08 10:02:47 -0500

    Fixed create_course module, by setting endpoint resource to 'courses'

--- quizzey-backend/terraform/api-gw.tf
+++ quizzey-backend/terraform/api-gw.tf
@@ -70,7 +70,7 @@ module "get_course" {
 module "create_course" {
   source          = "./gw-method-and-intg-resources"
   apigateway      = aws_api_gateway_rest_api.quizzey-api-gateway
-  resource        = aws_api_gateway_resource.course
+  resource        = aws_api_gateway_resource.courses
   lambda_function = aws_lambda_function.create_course_lambda
   authorization   = "NONE"
   httpmethod      = "POST"
