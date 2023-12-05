d539f2b5baf9141f688d2e525d0395e87d97a311
Juwan Gross <juwangross0815@gmail.com>
2023-12-05 08:09:30 -0500

    commit

--- quizzey-backend/lambdas/tables.py
+++ quizzey-backend/lambdas/tables.py
@@ -1,8 +1,7 @@
 import json
 import os
-import mysql.connector
 from mysql.connector import Error
-
+from db import DbUtils
 
 def all_tables_create_handler(event, context):
     host = os.environ.get('HOST')
@@ -11,25 +10,19 @@ def all_tables_create_handler(event, context):
     password = os.environ.get('PASSWORD')
 
     try:
-        connection = mysql.connector.connect(host=host, database=db_name, user=username, password=password)
-        cursor = connection.cursor()
+        with DbUtils(host, db_name, username, password) as db:
+            if db.is_connected():
+                db_info = db.get_server_info()
+                print("Connected to MySQL Server version:", db_info)
 
-        if connection.is_connected():
-            db_info = connection.get_server_info()
-            print("Connected to MySQL Server version:", db_info)
-
-            # TODO: Make changes to insert and update lambbdas in sets.py
-            cursor.execute("CREATE TABLE IF NOT EXISTS `courses`(`courseId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `courseName` VARCHAR(75), `organization` VARCHAR(150), `textbook` VARCHAR(150), `active` BOOLEAN, `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastModifiedDate` DATETIME(0))")
-            cursor.execute("CREATE TABLE IF NOT EXISTS `quizzey_sets`(`setId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, courseId INT NOT NULL, `setName` VARCHAR(175), `active` BOOLEAN, `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastModifiedDate` DATETIME(0), FOREIGN KEY(`courseId`) REFERENCES `courses`(`courseId`))")
-            cursor.execute("CREATE TABLE IF NOT EXISTS `questions`(`questionId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, setId INT NOT NULL, `question` VARCHAR(450), `answer` VARCHAR(450), `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastModifiedDate` DATETIME(0), FOREIGN KEY (`setId`) REFERENCES `quizzey_sets`(`setId`))")
-            print("Table creation has been completed.")
+                cursor = db.cursor()
+                # TODO: Make changes to insert and update lambbdas in sets.py
+                cursor.execute("CREATE TABLE IF NOT EXISTS `courses`(`courseId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `courseName` VARCHAR(75), `organization` VARCHAR(150), `textbook` VARCHAR(150), `active` BOOLEAN, `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastModifiedDate` DATETIME(0))")
+                cursor.execute("CREATE TABLE IF NOT EXISTS `quizzey_sets`(`setId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, courseId INT NOT NULL, `setName` VARCHAR(175), `active` BOOLEAN, `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastModifiedDate` DATETIME(0), FOREIGN KEY(`courseId`) REFERENCES `courses`(`courseId`))")
+                cursor.execute("CREATE TABLE IF NOT EXISTS `questions`(`questionId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, setId INT NOT NULL, `question` VARCHAR(450), `answer` VARCHAR(450), `createdBy` VARCHAR(100), `createdDate` DATETIME(0), `lastModifiedDate` DATETIME(0), FOREIGN KEY (`setId`) REFERENCES `quizzey_sets`(`setId`))")
+                print("Table creation has been completed.")
     except Error as e:
         print('Error while connecting to MySQL...', e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed.")
     return{
         "statusCode": 200,
         "body": json.dumps({'Success': 'Database creation process has completed. Double check if you tables were added correctly.'})
