a1d55f9377b0c8bf8d25d2d72d15f05ffe707d75
Juwan Gross <juwangross0815@gmail.com>
2023-12-05 13:49:52 -0500

    commit

--- quizzey-backend/lambdas/questions.py
+++ quizzey-backend/lambdas/questions.py
@@ -21,7 +21,7 @@ def get_questions_by_sId_handler(event, context):
                 db_info = db.get_server_info()
                 print("Connected to MySQL Server version:", db_info)
 
-                query = ("SELECT * FROM questions WHERE setId = %(set_id)s")
+                query = ("SELECT * FROM c WHERE setId = %(set_id)s")
                 cursor = db.cursor(dictionary=True)
                 cursor.execute(query, {'set_id': set_id})
                 rows = cursor.fetchall()
@@ -36,6 +36,48 @@ def get_questions_by_sId_handler(event, context):
         "body": json.dumps(rows, indent=3, default=str)
     }
 
+def create_new_question_handler(event, context):
+    # LOAD JSON LIST INTO PYTHON DICT
+    request_body = json.loads(event['body'])
+    created_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # CONNECT TO DB...
+    try:
+        with DbUtils(host, db_name, username, password) as db:
+            if db.is_connected():
+                db_info = db.get_server_info()
+                print("Connected to MySQL Server version:", db_info)
+                
+                query = ("INSERT INTO questions"
+                         "(setId, question, answer, createdBy, createdDate, lastModifiedDate)"
+                         "VALUES (%s, %s, %s, %s, %s, %s)")
+
+                cursor = db.cursor(dictionary=True)
+
+                # LOOP THROUGH DATA
+                for item in response_body:
+                    print(item['setId'])
+                    print(item['question'])
+                    print(item['answer'])
+                    print(item['createdBy'])
+                    print(created_date)
+                    
+
+                    data_for_query = (item['setId'], item['question'], item['answer'], item['createdBy'], createdDate, createdDate)
+                    cursor.execute(query, data_for_query)
+
+                db.commit()
+                print('COMMITTED NEW RECORD...')
+                cursor.close()
+                print('CURSOR CLOSED...')                
+                    
+    except Error as e:
+        print('Error while connecting to MySQL...', e)
+
+    return{
+        "statusCode": 200,
+        "body": json.dumps({'Success': 'Question creation process has completed. Double check if your new course record was added correctly.'}, indent=3)
+    }
 
 def question_update_handler(event, context):
     return{}
