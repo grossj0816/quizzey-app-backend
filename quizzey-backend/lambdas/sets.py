29780bbe019546a5f9a3f251bd05d4fad869cca9
Juwan Gross <juwangross0815@gmail.com>
2023-12-05 09:22:05 -0500

    commit

--- quizzey-backend/lambdas/sets.py
+++ quizzey-backend/lambdas/sets.py
@@ -82,6 +82,46 @@ def create_new_set_handler(event, context):
 
 
 def set_update_handler(event, context):
-    return{}
+    request_body = json.loads(event['body'])
+    set_id = request_body['setId']
+    set_name = request_body['setName']
+    active = True if request_body['active'] == 1 else False
+    created_by = request_body['createdBy']
+    created_date = request_body['createdDate']
+    created_date_obj = datetime.datetime.strptime(created_date, '%Y-%m-%d %H:%M:%S')
+    last_mod_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(set_id)
+    print(set_name)
+    print(active)
+    print(created_by)
+    print(created_date_obj)
+    print(last_mod_date)
+
+    try:
+        with DbUtils(host, db_name, username, password) as db:
+            if db.is_connected():
+                db_info = db.get_server_info()
+                print("Connected to MySQL Server version:", db_info)
+
+                if isinstance(set_name, str) and isinstance(active, bool) and isinstance(created_by, str):
+                    query = ("""UPDATE quizzey_sets
+                                SET setName=%s, active=%s, createdBy=%s, createdDate=%s, lastModifiedDate=%s
+                                WHERE setId=%s""")
+
+                    data_for_query = (set_name, active, created_by, created_date_obj, last_mod_date, set_id)
+                    cursor = db.cursor(dictionary=True)
+                    cursor.execute(query, data_for_query)
+                    db.commit()
+                    print('COMMITTED EXISTING RECORD UPDATE...')
+                    cursor.close()
+                    print('CURSOR CLOSED...')
+    except Error as e:
+        print('Error while connecting to MySQL...', e)
+
+    return{
+        "statusCode": 200,
+        "body": json.dumps({'Success', 'Set update process has completed. Double check if your new set record was added correctly.'}, indent=3)
+    }
 
 
