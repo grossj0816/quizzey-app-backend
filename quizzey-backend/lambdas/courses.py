6eee73d3fbdae6c4b844dda5e04796b525b153de
Juwan Gross <jgross@excelsior.edu>
2024-02-27 08:38:39 -0500

    Commit

--- quizzey-backend/lambdas/courses.py
+++ quizzey-backend/lambdas/courses.py
@@ -36,7 +36,9 @@ def courses_getter_handler(event, context):
     return{
         "statusCode": 200,
         "headers": {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
         }
         "body": json.dumps(rows, indent=3, default=str)
     }
