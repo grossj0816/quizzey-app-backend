e1178d85706c10a2ba414b3ef58dff10797e1101
Juwan Gross <jgross@excelsior.edu>
2024-02-27 08:12:37 -0500

    Added more headers for react.

--- quizzey-backend/lambdas/courses.py
+++ quizzey-backend/lambdas/courses.py
@@ -35,6 +35,11 @@ def courses_getter_handler(event, context):
             
     return{
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-AllowMethods"; "OPTIONS,GET"
+        }
         "body": json.dumps(rows, indent=3, default=str)
     }
 
