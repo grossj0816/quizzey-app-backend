40555c075bdeab8f99a7bd26641e3a10c32725ee
Juwan Gross <jgross@excelsior.edu>
2024-02-27 08:27:55 -0500

    commit'

--- quizzey-backend/lambdas/courses.py
+++ quizzey-backend/lambdas/courses.py
@@ -35,11 +35,6 @@ def courses_getter_handler(event, context):
             
     return{
         "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods"; "OPTIONS,GET"
-        }
         "body": json.dumps(rows, indent=3, default=str)
     }
 
