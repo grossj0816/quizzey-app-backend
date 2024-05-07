09c732cec571bd2b1a4c8083cd5132c566317889
Juwan Gross <jgross@excelsior.edu>
2024-05-07 14:43:43 -0400

    commit

--- quizzey-backend/terraform/gw-method-and-intg-resources/main.tf
+++ quizzey-backend/terraform/gw-method-and-intg-resources/main.tf
@@ -33,14 +33,16 @@ resource "aws_api_gateway_integration" "integration" {
 #The type set means that we are integrating with a lambda
  type                    = "AWS_PROXY"
  uri                     = var.lambda_function.invoke_arn  
+ content_handling        = "CONVERT_TO_TEXT" 
 }
 
 resource "aws_api_gateway_integration_response" "intgresponse" {
   depends_on             = [aws_api_gateway_integration.integration]
   rest_api_id            = var.apigateway.id 
-  resource_id             = var.resource.id
+  resource_id            = var.resource.id
   http_method            = var.httpmethod
-  status_code            =  aws_api_gateway_method_response.response.status_code
+  status_code            = aws_api_gateway_method_response.response.status_code
+  content_handling       = "CONVERT_TO_TEXT" 
   response_templates = {
     "application/json" = ""
   }
