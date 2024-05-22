a9cb210708ba1ab03aa9e07e413a7fd667d02bbe
Juwan Gross <jgross@excelsior.edu>
2024-05-22 09:16:13 -0400

    commit

--- quizzey-backend/terraform/api-gw.tf
+++ quizzey-backend/terraform/api-gw.tf
@@ -78,11 +78,6 @@ resource "aws_api_gateway_resource" "sets_by_courseId" {
   path_part   = "{courseId}" 
 }
 
-resource "aws_api_gateway_resource" "set" {
-  rest_api_id = aws_api_gateway_rest_api.quizzey-api-gateway.id
-  parent_id   = aws_api_gateway_resource.api.id
-  path_part   = "{setId}"
-}
 
 resource "aws_api_gateway_resource" "questions" {
   rest_api_id = aws_api_gateway_rest_api.quizzey-api-gateway.id
