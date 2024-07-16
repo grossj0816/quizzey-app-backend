4df058a115763c298f4cf44dc6bd5498dad3d343
Juwan Gross <jgross@excelsior.edu>
2024-07-16 14:25:33 -0400

    (Part 1) fixing endpoint issues with sets

--- quizzey-backend/terraform/api-gw.tf
+++ quizzey-backend/terraform/api-gw.tf
@@ -65,12 +65,12 @@ resource "aws_api_gateway_resource" "setId" {
 }
 
 
-resource "aws_api_gateway_resource" "sets" {
-  rest_api_id = aws_api_gateway_rest_api.quizzey-api-gateway.id
-  parent_id   = aws_api_gateway_rest_api.quizzey-api-gateway.root_resource_id
-  path_part   = "sets" 
+# resource "aws_api_gateway_resource" "sets" {
+#   rest_api_id = aws_api_gateway_rest_api.quizzey-api-gateway.id
+#   parent_id   = aws_api_gateway_rest_api.quizzey-api-gateway.root_resource_id
+#   path_part   = "sets" 
 
-}
+# }
 
 # resource "aws_api_gateway_resource" "set_by_setId" {
 #   rest_api_id = aws_api_gateway_rest_api.quizzey-api-gateway.id
